140fa6479fb144a33231aaa05ca97424a1684234
Turtvaiz <4550061+vaisest@users.noreply.github.com>
2021-10-09 15:40:59 +0300

    käyttöliittymän päivitys

--- pakkaus/__main__.py
+++ pakkaus/__main__.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 from . import huffman
+from . import lzw
 
 
 def simple_ui():
@@ -20,19 +21,48 @@ def simple_ui():
     print(f"Uudelleen purettu merkkijono on: {decoded_string}")
 
 
+def get_help():
+    print("Käyttö:")
+    print("  python -m pakkaus (huffman|lzw) (compress|uncompress) filename compressed_filename")
+    sys.exit()
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         simple_ui()
-    elif sys.argv[1] in ["c", "compress"]:
-        source_file = sys.argv[2]
-        destination_file = sys.argv[3]
-        huffman.pack_file(source_file, destination_file)
+    elif sys.argv[2] in ["-h", "--help"]:
+        get_help()
+    elif sys.argv[2] in ["c", "compress"]:
+        source_file = sys.argv[3]
+        destination_file = sys.argv[4]
+
+        print("Pakataan...")
+
+        if sys.argv[1] in ["h", "huf", "huffman"]:
+            huffman.pack_file(source_file, destination_file)
+        if sys.argv[1] in ["lzw", "lempel-ziv-welch"]:
+            lzw.compress_file(source_file, destination_file)
+        else:
+            get_help()
+
+        print("Pakattu")
+
         source_size = os.path.getsize(source_file)
         destination_size = os.path.getsize(destination_file)
         print(
-            f"Alkuperäisen koko: {source_size}, pakatun koko: {destination_size}, pakkaussuhde: {(destination_size/source_size)*100}%."
+            f"Alkuperäisen koko: {source_size} tavua, pakatun koko: {destination_size} tavua, pakkaussuhde: {(destination_size/source_size)*100}%."
         )
-    elif sys.argv[1] in ["u", "uncompress"]:
-        source_file = sys.argv[2]
-        destination_file = sys.argv[3]
-        huffman.unpack_file(source_file, destination_file)
+
+    elif sys.argv[2] in ["u", "uncompress"]:
+        source_file = sys.argv[3]
+        destination_file = sys.argv[4]
+        print("Puretaan...")
+
+        if sys.argv[1] in ["h", "huf", "huffman"]:
+            huffman.unpack_file(source_file, destination_file)
+        if sys.argv[1] in ["lzw", "lempel-ziv-welch"]:
+            lzw.uncompress_file(source_file, destination_file)
+        else:
+            get_help()
+
+        print("Purettu")
